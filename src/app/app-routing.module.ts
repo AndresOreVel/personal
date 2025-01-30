a4c5fabfb88d8a045e148d14381424873ba1511a
AndresOreVel <121023104+AndresOreVel@users.noreply.github.com>
2025-01-30 17:34:22 +0100

    new commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { EnglishComponent } from '../english/english.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'es', component: AppComponent },
+  // { path: 'en', component: EnglishComponent },
+  // { path: '', redirectTo: 'es', pathMatch: 'full'},
+  // { path: '**', redirectTo: 'es'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'folio';
+  theme: string | undefined;
+ 
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {
+    this.theme = localStorage.getItem('theme') || 'light';
+    this.applyTheme();
+  }
+
+
+  toggleTheme(){
+    this.theme = this.theme === 'light' ? 'dark' : 'light';
+    localStorage.setItem('theme', this.theme);
+    this.applyTheme();
+  }
+
+  applyTheme(){
+    if(this.theme === 'dark'){
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme');
+      document.getElementById('theme-icon')?.setAttribute('src', '../assets/images/light.png');
+    }else{
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
+      document.getElementById('theme-icon')?.setAttribute('src', '../assets/images/dark.png');
+    }
+  }
+  
+
 }
